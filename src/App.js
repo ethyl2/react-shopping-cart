061cc8378353f2e18ba385141f346d33f334f938
Heather Nuffer <zzzuzz@gmail.com>
2019-12-06 16:48:32 -0700

    CartContext now provides data to Products and ShoppingCart components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-import { ProductContext  }from './contexts/ProductContext';
+import { ProductContext  } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -20,7 +21,8 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}} >
-				<Navigation cart={cart} />
+				<CartContext.Provider value={cart} >
+				<Navigation />
 
 				{/* Routes */}
 				<Route
@@ -31,8 +33,9 @@ function App() {
 
 				<Route
 					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
+					component={ShoppingCart}
 				/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
